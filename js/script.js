571cbe79df63bf00b61889ed0163e0fbf33cde84
psanchezher <pamemlachristina@mac.com>
2021-03-22 14:00:43 -0700

    added more regex

--- js/script.js
+++ js/script.js
@@ -136,7 +136,7 @@ function emailVerify (email) {
 // regex formula from  www.regex101.com
 function ccNumberVerify (ccBox) {
     const ccNumberVal = ccBox.getElementsByTagName('INPUT')[10].value;
-    const ccNumberIsValid  = /(^4[0-9]{12}(?:[0-9]{3})?$)|(^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$)|(3[47][0-9]{13})|(^3(?:0[0-5]|[68][0-9])[0-9]{11}$)|(^6(?:011|5[0-9]{2})[0-9]{12}$)|(^(?:2131|1800|35\d{3})\d{11}$)/.test(ccNumberVal);
+    const ccNumberIsValid  = /^[0-9]{16}(?:-[0-9]{13})?$/.test(ccNumberVal);
     return ccNumberIsValid; 
 };
 // regex formula from  www.regex101.com
